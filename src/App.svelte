80e7eda6692fdf15ad37ef36bdef084326773eba
vik_kod <vik_kod@mail.ru>
2019-05-20 12:44:01 +0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  import page from "page";
+  let route = false;
+
+  page("/", () => {
+    import(/* webpackChunkName: "main-page" */ "./pages/MainPage.svelte").then(
+      m => {
+        route = m.default;
+      }
+    );
+  });
+
+  page("/about", () => {
+    import(/* webpackChunkName: "about" */ "./pages/About.svelte").then(m => {
+      route = m.default;
+    });
+  });
+
+  page();
+</script>
+
+<style>
+  @import url("./assets/styles/main.pcss");
+</style>
+
+<nav>
+  <img src="./assets/images/svelte.png" alt="">
+  <a href="/about">about</a>
+</nav>
+
+<svelte:component this={route} />
---
+++ webpack.config.js
@@ -0,0 +1,115 @@
+const path = require('path')
+const fs = require('fs')
+const CopyPlugin = require('copy-webpack-plugin')
+const PreloadWebpackPlugin = require('preload-webpack-plugin')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+const mode = process.env.NODE_ENV || 'development'
+const prod = mode === 'production'
+const pkg = require('./package.json')
+
+module.exports = env => {
+  return {
+    stats: 'minimal',
+    mode: 'development',
+    entry: {
+      app: './src/main.js',
+    },
+    resolve: {
+      extensions: ['.mjs', '.js', '.svelte', '.json'],
+      alias: {
+        '@': './src',
+      },
+    },
+    devServer: {
+      historyApiFallback: true,
+      contentBase: path.join(__dirname, 'build'),
+      compress: true,
+      port: 9000,
+      open: true,
+      overlay: {
+        warnings: true,
+        errors: true,
+      },
+    },
+    output: {
+      path: __dirname + '/build',
+      filename: '[name].[contenthash].js',
+      chunkFilename: '[name].[contenthash].js',
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /node_modules/,
+            chunks: 'initial',
+            name: 'vendor',
+            enforce: true,
+          },
+        },
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.svelte$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'svelte-loader',
+            options: {
+              emitCss: true,
+              hotReload: true,
+            },
+          },
+        },
+        {
+          test: /('.mjs|.js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader?cacheDirectory=true',
+          },
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.pcss$/,
+          use: ['postcss-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(gif|png|jpe?g|svg|woff|ttf)$/,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                limit: 4000,
+                name: '[path][name].[ext]',
+              },
+            },
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+      }),
+      new CopyPlugin([{from: './src/static', to: './'}]),
+      new HtmlWebpackPlugin({
+        title: pkg.name,
+        template: './index.html',
+      }),
+      new PreloadWebpackPlugin(),
+    ],
+    devtool: prod ? false : 'source-map',
+  }
+}
